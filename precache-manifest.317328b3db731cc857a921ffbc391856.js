98333a2ab194a1265a8cae39e855f467693e3285
cycycy <galoncyryll@gmail.com>
2019-01-04 00:45:09 -0800

    Updates

---
+++ precache-manifest.317328b3db731cc857a921ffbc391856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edaee91c4a20907114d3286853c910a2",
+    "url": "/website-portfolio/static/media/Baron Neue.edaee91c.woff"
+  },
+  {
+    "revision": "3a5481507d16cd67b158",
+    "url": "/website-portfolio/static/js/runtime~main.3a548150.js"
+  },
+  {
+    "revision": "23b6c41650828f4e1976",
+    "url": "/website-portfolio/static/js/main.23b6c416.chunk.js"
+  },
+  {
+    "revision": "89038790140d02bac95a",
+    "url": "/website-portfolio/static/js/1.89038790.chunk.js"
+  },
+  {
+    "revision": "23b6c41650828f4e1976",
+    "url": "/website-portfolio/static/css/main.c05f3cd3.chunk.css"
+  },
+  {
+    "revision": "e1e3ade5c41cdfb72dfba23c7a645eb9",
+    "url": "/website-portfolio/index.html"
+  }
+];
