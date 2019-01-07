99d89efb36009a653bb6ed62a64af30416d53d96
cycycy <galoncyryll@gmail.com>
2019-01-06 18:55:17 -0800

    fixed mobile hover bugs

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -9,16 +9,16 @@ class Contact extends Component {
                 <p>Get in touch with me!</p>
                 <div className='socials'>
                     <a className='btn' href='https://github.com/galoncyryll' target='_blank' rel="noopener noreferrer">
-                        <i class="fab fa-github" />
+                        <i className="fab fa-github" />
                     </a>
                     <a className='btn' href='https://www.linkedin.com/in/cyryll-joseph-galon-461710147/' target='_blank' rel="noopener noreferrer">
-                        <i class="fab fa-linkedin-in" />
+                        <i className="fab fa-linkedin-in" />
                     </a>
                     <a className='btn' href='mailto:galoncyryll@gmail.com'>
-                        <i class="fas fa-at" />
+                        <i className="fas fa-at" />
                     </a>
                     <a className='btn' href='#0'>
-                        <i class="fas fa-phone" />
+                        <i className="fas fa-phone" />
                     </a>       
                 </div>
             </div>
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -7,6 +7,11 @@ class Projects extends Component {
     componentDidMount(){
         new WOW.WOW().init();
     }
+
+    onClickDiv = () => {
+        return true;
+    }
+
     render () {
         return (
             <div className='projects-body' id='projects'>
@@ -14,7 +19,7 @@ class Projects extends Component {
                 <div className='projects-container'>
                     <div>
                         <h2>Face Recognition App</h2>
-                        <div onClick='' className='project-card1 wow fadeIn' data-wow-delay='0.1s'>
+                        <div onClick={this.onClickDiv} className='project-card1 wow fadeIn' data-wow-delay='0.1s'>
                             <div className='overlay'>
                                 <a href='#viewproject'>GitHub</a>
                                 <a href='#viewproject'>Live Demo</a>
@@ -23,7 +28,7 @@ class Projects extends Component {
                     </div>
                     <div>
                         <h2>Portfolio</h2>
-                        <div onClick='' className='project-card2 wow fadeIn' data-wow-delay='0.2s'>
+                        <div onClick={this.onClickDiv} className='project-card2 wow fadeIn' data-wow-delay='0.2s'>
                             <div className='overlay'>
                                 <a href='#viewproject'>GitHub</a>
                                 <a href='#viewproject'>Live Demo</a>
@@ -32,7 +37,7 @@ class Projects extends Component {
                     </div>
                    <div>
                         <h2>Robofriends</h2>
-                        <div onClick='' className='project-card3 wow fadeIn' data-wow-delay='0.3s'>
+                        <div onClick={this.onClickDiv} className='project-card3 wow fadeIn' data-wow-delay='0.3s'>
                             
                             <div className='overlay'>
                                 <a href='#viewproject'>GitHub</a>
