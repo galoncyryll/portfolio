715d8cb29e4ef41a7dc6b3babed9c557e30d4f7d
cycycy <galoncyryll@gmail.com>
2019-01-13 22:09:27 -0800

    completely changed projects card and built a dynamic reusable project card component

---
+++ src/components/projects/projectCard.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './projectCard.css';
+
+
+class ProjectCard extends Component {
+    constructor (props){
+        super()
+    }
+    render () {
+        return (
+            <div className="project-card">
+                <div className="meta">
+                <div className="photo" style={{backgroundImage: `url(${this.props.bgImage})`}}></div>
+                <ul className="details">
+                    <li className="tags">
+                    <ul>
+                        <li><i className="fas fa-code" /> Tools/Techs Used</li>
+                        {
+                            this.props.tech.map((tech, i) =>{
+                                return (
+                                    <li key={i}>{tech}</li>
+                                );
+                            })
+                        }
+                    </ul>
+                    </li>
+                </ul>
+                </div>
+                <div className="description">
+                <h2>{this.props.title}</h2>
+                <p>{this.props.description}</p>
+                <p className="project-links">
+                    <a href={this.props.GitHub} target='_blank' rel="noopener noreferrer">GitHub</a>
+                    {
+                        this.props.Demo 
+                        ? <a href={this.props.Demo}target='_blank' rel="noopener noreferrer">Live Demo</a>
+                        : false
+                    }  
+                </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ProjectCard;
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import './projects.css';
+import ProjectCards from './projectCard';
+
+import Brain from './img/brain.PNG';
+import Crim from './img/Crim.png';
+import Portfolio from './img/portfolio.PNG';
+import DiscordBanner from './img/discordbanner.png';
 
 class Projects extends Component {
 
@@ -12,53 +18,37 @@ class Projects extends Component {
             <div className='projects-body' id='projects'>
                 <h1>Projects</h1>
                 <div className='projects-container'>
-                    <div>
-                        <h2>CRIM</h2>
-                        <div onClick={this.onClickDiv} className='project-card1 wow fadeIn' data-wow-delay='0.1s'>
-                            <div className='overlay'>
-                                <a href='https://github.com/galoncyryll/CRIM' target='_blank' rel="noopener noreferrer">GitHub</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2>Face Recognition App</h2>
-                        <div onClick={this.onClickDiv} className='project-card2 wow fadeIn' data-wow-delay='0.2s'>
-                            <div className='overlay'>
-                                <a href='https://github.com/galoncyryll/face-recognition-app' target='_blank' rel="noopener noreferrer">GitHub</a>
-                                <a href='https://face-recognition-p.herokuapp.com/' target='_blank' rel="noopener noreferrer">Live Demo</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2>Portfolio</h2>
-                        <div onClick={this.onClickDiv} className='project-card3 wow fadeIn' data-wow-delay='0.3s'>
-                            <div className='overlay'>
-                                <a href='https://github.com/galoncyryll/website-portfolio' target='_blank' rel="noopener noreferrer">GitHub</a>
-                                <a href='#0'>Live Demo</a>
-                            </div>
-                        </div>
-                    </div>
-                   <div>
-                        <h2>Robofriends</h2>
-                        <div onClick={this.onClickDiv} className='project-card4 wow fadeIn' data-wow-delay='0.4s'>
-                            
-                            <div className='overlay'>
-                                <a href='https://github.com/galoncyryll/robofriends' target='_blank' rel="noopener noreferrer">GitHub</a>
-                                <a href='https://galoncyryll.github.io/robofriends/' target='_blank' rel="noopener noreferrer">Live Demo</a>   
-                            </div>
-                        </div>
-                   </div>
-                   <div>
-                        <h2>cycycy bot (Discord bot)</h2>
-                        <div onClick={this.onClickDiv} className='project-card5 wow fadeIn' data-wow-delay='0.5s'>
-                            
-                            <div className='overlay'>
-                                <a href='https://github.com/galoncyryll/cycycy-bot' target='_blank' rel="noopener noreferrer">GitHub</a>
-                                {/* <a href='https://galoncyryll.github.io/robofriends/' target='_blank' rel="noopener noreferrer">Live Demo</a>    */}
-                            </div>
-                        </div>
-                   </div>
-                </div>       
+                    <ProjectCards 
+                        title='CRIM'
+                        bgImage={Crim}
+                        description={`An app for providing reservations and storing dental patients information for the CEU Dentistry Dept.`}
+                        tech={['HTML5/CSS3', 'Javascript', 'Java', 'MySQL']}
+                        GitHub='https://github.com/galoncyryll/CRIM'
+                    />
+                    <ProjectCards 
+                        title='Face Recognition App'
+                        bgImage={Brain}
+                        description={`A simple app that detects face from the user's input and records the entries to Postgres Database.`}
+                        tech={['HTML5/CSS3', 'Javascript', 'ReactJS', 'NodeJS', 'ExpressJS']}
+                        GitHub='https://github.com/galoncyryll/face-recognition-app'
+                        Demo='https://face-recognition-p.herokuapp.com/'
+                    />
+                    <ProjectCards 
+                        title='Portfolio'
+                        bgImage={Portfolio}
+                        description={`A website to showcase my skills, projects and my interests for my career. (Constantly updating projects section)`}
+                        tech={['HTML5/CSS3', 'Javascript', 'ReactJS']}
+                        GitHub='https://github.com/galoncyryll/website-portfolio'
+                        Demo='#0'
+                    />
+                    <ProjectCards 
+                        title='cycycy Bot (Discord Bot)'
+                        bgImage={DiscordBanner}
+                        description={`A bot to moderate my own discord server. (Bot is in early stages and still in development)`}
+                        tech={['NodeJS', 'DiscordJS', 'Javascript']}
+                        GitHub='https://github.com/galoncyryll/cycycy-bot'
+                    />
+                </div>     
             </div> 
         );   
     }
