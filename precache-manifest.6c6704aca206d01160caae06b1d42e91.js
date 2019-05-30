2555971fd26d06f08349f1fabe9368092bb0e4bd
Cyryll Galon <galoncyryll@gmail.com>
2019-05-30 10:41:01 -0700

    Updates

---
+++ precache-manifest.6c6704aca206d01160caae06b1d42e91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc3494a83c66c0b35fe9cd23b5c48e6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "547ff7a8107868a7a6e2",
+    "url": "/portfolio/static/css/main.b7d0d3ce.chunk.css"
+  },
+  {
+    "revision": "47752f81323c8ca4d9a3",
+    "url": "/portfolio/static/js/2.dbc365ad.chunk.js"
+  },
+  {
+    "revision": "547ff7a8107868a7a6e2",
+    "url": "/portfolio/static/js/main.6468191b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "edaee91c4a20907114d3286853c910a2",
+    "url": "/portfolio/static/media/Baron Neue.edaee91c.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23f6d7f2f2f03cb9e3fb0bc2b59603e2.js"
+  "/portfolio/precache-manifest.6c6704aca206d01160caae06b1d42e91.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
