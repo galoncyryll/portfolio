7977ebef44644cb451fcac82f857c077753aef3c
Cyryll Galon <galoncyryll@gmail.com>
2019-07-25 18:31:00 -0700

    Updates

---
+++ precache-manifest.fcdce307dc408508a953455760a8a2e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edaee91c4a20907114d3286853c910a2",
+    "url": "/static/media/Baron Neue.edaee91c.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8d253f37916fb745a37",
+    "url": "/static/js/main.e8d253f3.chunk.js"
+  },
+  {
+    "revision": "93e418e647761c78e1dc",
+    "url": "/static/js/1.93e418e6.chunk.js"
+  },
+  {
+    "revision": "e8d253f37916fb745a37",
+    "url": "/static/css/main.9d92e6da.chunk.css"
+  },
+  {
+    "revision": "9902b4ea32ec81ea41de21a25e073371",
+    "url": "/index.html"
+  }
+];
